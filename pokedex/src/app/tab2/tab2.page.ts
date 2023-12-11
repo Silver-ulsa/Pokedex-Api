12def62873b258b72ccbc4fbc1b038f5b20d5390
Silver-ulsa <20190198@ulsavictoria.edu.mx>
2023-12-10 21:01:49 -0600

    10/12/2023 (2)

--- pokedex/src/app/tab2/tab2.page.ts
+++ pokedex/src/app/tab2/tab2.page.ts
@@ -47,4 +47,95 @@ export class Tab2Page implements OnInit {
       pokemon.types.some((type: any) => type.type.name.toLowerCase().includes(term))
     );
   }
+
+  filters(){
+    console.log("Algun día haré los filtros");
+  }
+
+  getColorByType(type: string){ 
+    switch(type){ 
+      case 'grass': 
+        return '#38BF4B'; 
+      case 'poison': 
+        return '#B567CE'; 
+      case 'fire': 
+        return '#FF9741'; 
+      case 'flying': 
+        return '#89AAE3'; 
+      case 'water': 
+        return '#3692DC'; 
+      case 'bug': 
+        return '#83C300'; 
+      case 'normal': 
+        return '#919AA2'; 
+      case 'electric': 
+        return '#FFCC00'; 
+      case 'ground': 
+        return '#E87236'; 
+      case 'fairy': 
+        return '#FB89EB'; 
+      case 'fighting': 
+        return '#E0306A'; 
+      case 'psychic': 
+        return '#FF6675'; 
+      case 'rock': 
+        return '#C8B686'; 
+      case 'steel': 
+        return '#5A8EA2'; 
+      case 'ice': 
+        return '#4CD1C0'; 
+      case 'ghost': 
+        return '#4C6AB2'; 
+      case 'dragon': 
+        return '#7038f8'; 
+      case 'dark': 
+        return '#705848';
+      default:
+          return '#000';
+    }
+  }
+
+  getColorByTypeChip(type: string){ 
+    switch(type){ 
+      case 'grass': 
+        return '#35A349'; 
+      case 'poison': 
+        return '#9343AD'; 
+      case 'fire': 
+        return '#D48341'; 
+      case 'flying': 
+        return '#6687C1'; 
+      case 'water': 
+        return '#337FBD'; 
+      case 'bug': 
+        return '#71A60D'; 
+      case 'normal': 
+        return '#6E7881'; 
+      case 'electric': 
+        return '#fae078'; 
+      case 'ground': 
+        return '#C95A22'; 
+      case 'fairy': 
+        return '#D066C1'; 
+      case 'fighting': 
+        return '#B22755'; 
+      case 'psychic': 
+        return '#CB3F4D'; 
+      case 'rock': 
+        return '#8C7841'; 
+      case 'steel': 
+        return '#42768A'; 
+      case 'ice': 
+        return '#2CAE9E'; 
+      case 'ghost': 
+        return '#37518F'; 
+      case 'dragon': 
+        return '#566fbe'; 
+      case 'dark': 
+      return '#A29288';
+      default:
+        return '#000';
+    }
+  }
+
 }
